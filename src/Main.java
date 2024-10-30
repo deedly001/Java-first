81a9034402e7174effff6b2f6e840dd8adf94727
deedly001 <ariasmall@yandex.ru>
2024-10-30 13:55:47 +0300

    HW-10.1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,61 @@
-
 public class Main {
-        public static void main(String[] args) {
-            System.out.println("Привет Мир!");
-            System.out.println("Hello World!");
-            System.out.println("Hello Java!");
-        }
+
+  public static void main(String[] args) {
+    System.out.println("Задание 1. \n");
+    isLeapYear(2024);
+    isLeapYear(2023);
+    isDasher();
+
+    System.out.println("Задание 2. \n");
+    whichOS("Android", 2024);
+    whichOS("iOS", 2014);
+    isDasher();
+
+    System.out.println("Задание 3. \n");
+    deliveryCard(110);
+  }
+
+  public static void isDasher() {
+    System.out.println("\n-----------------------------------\n");
+  }
+
+  public static void isLeapYear(int year) {
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 != 0)) {
+      System.out.println(year + " год — високосный год");
+    } else {
+      System.out.println(year + " год — не високосный год");
+    }
+  }
+
+  public static void whichOS(String typeOS, int yearOfDevice) {
+    if (typeOS.equalsIgnoreCase("Android") && (yearOfDevice < 2015)) {
+      System.out.println("Установите облегченную версию приложения для Android по ссылке");
+    } else if (typeOS.equalsIgnoreCase("Android") && (yearOfDevice >= 2015)) {
+      System.out.println("Установите версию приложения для Android по ссылке");
+    } else if (typeOS.equalsIgnoreCase("IOS") && (yearOfDevice < 2015)) {
+      System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+    } else if (typeOS.equalsIgnoreCase("IOS") && (yearOfDevice >= 2015)) {
+      System.out.println("Установите версию приложения для IOS по ссылке");
+    } else {
+      throw new RuntimeException("Вы ввели что-то другое, попробуйте снова");
+    }
+  }
+
+
+  public static void deliveryCard(int distanceToDelivery) {
+    byte daysForDelivery = 0;
+
+    if (distanceToDelivery <= 20) {
+      daysForDelivery += 1;
+      System.out.println("На доставку потребуется дней: " + daysForDelivery);
+    } else if (distanceToDelivery <= 60) {
+      daysForDelivery += 2;
+      System.out.println("На доставку потребуется дней: " + daysForDelivery);
+    } else if (distanceToDelivery <= 100) {
+      daysForDelivery += 3;
+      System.out.println("На доставку потребуется дней: " + daysForDelivery);
+    } else {
+      System.out.println("Свыше 100 км доставки нет");
+    }
+  }
 }
