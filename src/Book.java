afc5a322d0151b5f4dbb91aea54f52ee92f5bd81
deedly001 <ariasmall@yandex.ru>
2024-11-05 22:30:57 +0300

    HW12

---
+++ src/Book.java
@@ -0,0 +1,34 @@
+public class Book {
+
+  private String bookName;
+  private Author bookAuthor;
+  private int publishingYear;
+
+  public Book(String bookName, Author bookAuthor, int publishingYear) {
+    this.bookName = bookName;
+    this.publishingYear = publishingYear;
+    this.bookAuthor = bookAuthor;
+  }
+
+  public String getBookName() {
+    return this.bookName;
+  }
+
+  public void setBookName(String name) {
+    this.bookName = name;
+  }
+
+  public String getBookAuthor() {
+    return bookAuthor.getAuthor();
+  }
+
+  public int publishingYear() {
+    return this.publishingYear;
+  }
+
+  public void setPublishingYear(int age) {
+    this.publishingYear = age;
+  }
+
+}
+
